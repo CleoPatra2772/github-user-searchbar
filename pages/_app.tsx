31075c247117374a8e5f6e1366290bb1c5a85fd7
Cleo Gao <celeo127@outlook.com>
2022-09-20 16:27:56 +0800

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.css';
+import SSRProvider from 'react-bootstrap/SSRProvider';
+import { Header } from '../components/Header.component';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <SSRProvider>
+     <Header />
+    <Component {...pageProps} />
+   
+  </SSRProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/search.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next"
+
+const Search: NextPage = () => {
+
+    return(
+        <h1>I am search bar</h1>
+    )
+}
+
+
+export default Search;
